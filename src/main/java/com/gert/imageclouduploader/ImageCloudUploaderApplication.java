90ec2c61842843e27f1795f42b0ab9caf6b1caf4
Joakim Eskils <joakim.eskils@bricco.se>
2021-05-27 19:03:00 +0200

    Spring initlizr

---
+++ src/main/java/com/gert/imageclouduploader/ImageCloudUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.gert.imageclouduploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCloudUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageCloudUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gert/imageclouduploader/ImageCloudUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gert.imageclouduploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCloudUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
